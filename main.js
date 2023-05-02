e68b16b6ece34a8e96ab645e74394ba95ab348be
fhruby <haroodo.7@gmail.com>
2023-05-02 19:10:18 +0900

    unsandboxed

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
-(function(Scratch) {
+/*(function(Scratch) {
     'use strict';
 
     if (!Scratch.extensions.unsandboxed) {
         throw new Error('This Hello World example must run unsandboxed');
-    }
+    }*/
 
-    class DataOperationPlusPlus {
-        getInfo() {
-            return {
-                id: 'dataoperationpp',
-                name: "Let's operation some data.",
-                docsURI: 'https://github.com/forestrharumaki/tw-ext',
-                blocks: [{
-                        opcode: 'spritWithChar',
-                        blockType: Scratch.BlockType.REPORTER,
-                        text: 'Sprit [BEFORE] with [DELIMITER]',
-                        arguments: {
-                            BEFORE: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: 'I like apple'
-                            },
-                            DELIMITER: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: ' '
-                            }
-                        }
-                    },
-                    {
-                        opcode: 'getIndexItemFromArrayString',
-                        blockType: Scratch.BlockType.REPORTER,
-                        text: 'item [INDEX] of [BEFORE]',
-                        arguments: {
-                            BEFORE: {
-                                type: Scratch.ArgumentType.STRING,
-                                defaultValue: '[I,like,apple]'
-                            },
-                            INDEX: {
-                                type: Scratch.ArgumentType.NUMBER,
-                                defaultValue: 3
-                            }
+class DataOperationPlusPlus {
+    getInfo() {
+        return {
+            id: 'dataoperationpp',
+            name: "Let's operation some data.",
+            docsURI: 'https://github.com/forestrharumaki/tw-ext',
+            blocks: [{
+                    opcode: 'spritWithChar',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Sprit [BEFORE] with [DELIMITER]',
+                    arguments: {
+                        BEFORE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'I like apple'
+                        },
+                        DELIMITER: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: ' '
                         }
                     }
-                ]
-            };
-        }
-
-        spritWithChar(args) {
-            return '[' + ((args.BEFORE).split(args.DELIMITER)).join(',') + ']';
-        }
-
-        getIndexItemFromArrayString(args) {
-            return (args.BEFORE).charAt(0) == '[' && (args.BEFORE).charAt((args.BEFORE).length) == ']' ? (((args.BEFORE).substring(1, (args.BEFORE).length - 1)).split(','))[args.INDEX] : 'NaN';
-        }
+                },
+                {
+                    opcode: 'getIndexItemFromArrayString',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'item [INDEX] of [BEFORE]',
+                    arguments: {
+                        BEFORE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: '[I,like,apple]'
+                        },
+                        INDEX: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 3
+                        }
+                    }
+                }
+            ]
+        };
     }
 
-    Scratch.extensions.register(new DataOperationPlusPlus());
-})(Scratch);
+    spritWithChar(args) {
+        return '[' + ((args.BEFORE).split(args.DELIMITER)).join(',') + ']';
+    }
+
+    getIndexItemFromArrayString(args) {
+        return (args.BEFORE).charAt(0) == '[' && (args.BEFORE).charAt((args.BEFORE).length) == ']' ? (((args.BEFORE).substring(1, (args.BEFORE).length - 1)).split(','))[args.INDEX] : 'NaN';
+    }
+}
+
+Scratch.extensions.register(new DataOperationPlusPlus());
+/*})(Scratch);*/
